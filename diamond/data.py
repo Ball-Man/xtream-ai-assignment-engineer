07a9c273c360912bbc0a5cb7aeadcff8841c11cb
Francesco <francesco.mistri98@gmail.com>
2024-03-28 22:55:30 +0100

    Reverse order of colors and clarities in encoders
    This is done just for consistency with the cut encoder, which goes from the least desirable to the most desirable value.

--- diamond/data.py
+++ diamond/data.py
@@ -6,9 +6,9 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 
 CUT_GRADES = 'Poor', 'Fair', 'Good', 'Very Good', 'Premium', 'Ideal'
-COLORS = tuple(ascii_uppercase[3:])
-CLARITIES = ('FL', 'IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1',  'SI2', 'I1', 'I2',
-             'I3')
+COLORS = tuple(reversed(ascii_uppercase[3:]))
+CLARITIES = ('I3', 'I2', 'I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1',
+             'IF', 'FL')
 
 
 def load_raw(file_or_path) -> pd.DataFrame:
