0d7af2e068b57dccc05176e811551932b7acc76a
Francesco <francesco.mistri98@gmail.com>
2024-03-28 22:29:43 +0100

    Add data encoder for cut grades

--- diamond/data.py
+++ diamond/data.py
@@ -1,7 +1,15 @@
 """Data pipeline components."""
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
+
+CUT_GRADES = 'Poor', 'Fair', 'Good', 'Very Good', 'Premium', 'Ideal'
 
 
 def load_raw(file_or_path) -> pd.DataFrame:
     """Load raw dataset from file (csv only, so far)."""
     return pd.read_csv(file_or_path)
+
+
+cut_grades_encoder = OrdinalEncoder(categories=[list(CUT_GRADES)],
+                                    dtype=np.float32)
--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,8 @@ For simplicity, test cases are not documented through docstrings. Yet,
 they should be semantically very clear.
 """
 import pytest
+import pandas as pd
+import numpy as np
 
 from context import diamond         # NOQA
 from diamond import data
@@ -19,3 +21,10 @@ def raw_filename() -> str:
 def test_load_raw(raw_filename):
     df = data.load_raw(raw_filename)
     assert (df.count() > 0).all()
+
+
+def test_cut_grades_encoder():
+    cut_column = data.cut_grades_encoder.fit_transform(
+        np.array(data.CUT_GRADES).reshape(-1, 1))
+
+    assert (cut_column[:, 0] == range(6)).all()
